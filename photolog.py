666304eb98be1a09075911ec72bb6154b92a022b
Jou Sung-Shik <liks79@gmail.com>
2013-06-10 07:14:38 +0900

    Initial Commit

---
+++ photolog.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+   photolog.py
+    ~~~~~~~~~~~~~~
+
+    JPEG 사진에서 EXIF(교환 이미지 파일 형식, EXchangable Image File format)를 이용하여,
+    PhotoLog에서 사용할 위치기반 정보를 얻어 온다.
+
+    :copyright: (c) 2013 by liks79 [http://www.github.com/liks79]
+    :license: MIT LICENSE 2.0, see license for more details.
+"""
+
+import os
+from application import app
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
