715b515a69f19daad9317d8c457445ed4328daeb
Jou Sung-Shik <liks79@gmail.com>
2013-07-02 17:34:07 +0900

    photo 테이블에 데이터 저장 추가

--- photolog/controller/upload_files.py
+++ photolog/controller/upload_files.py
@@ -16,8 +16,14 @@ from flask import request, redirect, url_for, current_app, send_from_directory \
 				, render_template
 from werkzeug.utils import secure_filename
 
+from photolog.database import DBManager
+from photolog.model.photo import Photo
+from photolog.controller.login import login_required
+from photolog.exif_reader import EXIFReader
+
 from .. import photolog
 from .login import login_required
+from datetime import datetime
 
 
 ALLOWED_EXTENSIONS = set(['txt', 'doc', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
@@ -49,16 +55,32 @@ def upload_file():
 			filename = file.filename
 			print "filename : %s" % filename
 			filename = file.filename
-			upload_folder = os.path.join(current_app.root_path, 
-										current_app.config['UPLOAD_FOLDER'])
+			upload_folder = os.path.join(current_app.root_path, current_app.config['UPLOAD_FOLDER'])
 			print "root_path : %s" % current_app.root_path
 			print "upload_folder : %s" % upload_folder
 			file.save(os.path.join(upload_folder, filename))
-			return redirect(url_for('.upload', filename=filename))
+			
+			userid = request.form['userid']			
+			tag = request.form['tag']			
+			comment = request.form['comment']			
+			exif = EXIFReader(upload_folder + filename)
+			geotag_lat = exif.get_geotag_lat()
+			geotag_lng = exif.get_geotag_lng()
+			upload_date = datetime.today()
+			taken_date = datetime.today()
+            
+			print "geotag_lat: %s" % geotag_lat 
+           
+			photo = Photo(userid, tag, comment, geotag_lat, geotag_lng, upload_date, taken_date)
+			DBManager.db_session.add(photo)
+			DBManager.db_session.commit()
+
+			return render_template('main.html', name=filename)
 		else:
 			raise Exception("No file or support file extensions")
 	except Exception as e:
 		print "Upload error : %s" % str(e)
+        render_template('500.html')
 
 
 @photolog.route('/download/<filename>')
