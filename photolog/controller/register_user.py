81f5c3426352bce97b49ee2c476464ef50a53f01
Keaton Hong <keatonh@gmail.com>
2013-07-11 12:07:04 +0900

    사용자 정보 수정 시, 서버에서 폼 필드값 확인 로직 추

--- photolog/controller/register_user.py
+++ photolog/controller/register_user.py
@@ -45,7 +45,7 @@ def register_user(user=None):
                 # 성공적으로 사용자 등록이 되면, 로그인 화면으로 이동.
                 return redirect(url_for('.login'))
         else:
-            error = "password confirmation failed!"
+            error = "패스워드가 일치하지 않습니다. 다시 입력해주세요."
             print "Register user error : %s" % error
             return render_template('regist.html', error=error)
     else:
@@ -63,30 +63,39 @@ def modify_user(user=None):
         password = request.form['password']
         password_confirm = request.form['password_confirm']
         
+        try:
+            old_user = DBManager.db_session.query(User).filter_by(username=user).first()
+    
+            print old_user 
+        except Exception as e:
+            modify_error = "DB error occurs : " + str(e)
+            from photolog import server_error
+            server_error(500)
+                
         if password == '' or password_confirm == '':
-            modify_error = "not allowed blank for password "
+            modify_error = "패스워드에 공백은 허용하지 않습니다."
             print modify_error
-            return render_template('regist.html', error=modify_error)    
+            return render_template('regist.html', user=old_user, error=modify_error)    
         if password == password_confirm:
             print "modifying user info..."
             try:
-                old_user = DBManager.db_session.query(User).filter_by(username=user).first()
+#                 old_user = DBManager.db_session.query(User).filter_by(username=user).first()
                 old_user.password = password
                 
                 DBManager.db_session.commit()
     
-                print user 
+#                 print old_user 
             except Exception as e:
                 modify_error = "DB error occurs : " + str(e)
                 print modify_error
-                return render_template('regist.html', error=modify_error)
+                return render_template('regist.html', user=old_user, error=modify_error)
             else:
                 # 성공적으로 사용자 등록이 되면, 로그인 화면으로 이동.
                 return redirect(url_for('.login'))
         else:
-            modify_error = "password confirmation failed!"
+            modify_error = "패스워드가 일치하지 않습니다. 다시 입력해주세요."
             print "Register user error : %s" % modify_error
-            return render_template('regist.html', error=modify_error)
+            return render_template('regist.html', user=old_user, error=modify_error)
     
     else:
         # 사용자 정보 수정 화면
