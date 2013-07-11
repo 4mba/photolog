0a5d314e3c180beb2dec954e59a75ff8ee56260b
Jou Sung-Shik <liks79@gmail.com>
2013-07-11 16:28:08 +0900

    메소드 인터페이스 수정, KeyError 예외처리

--- photolog/exif_reader.py
+++ photolog/exif_reader.py
@@ -17,8 +17,9 @@ import EXIF;
 
 class EXIFReader :
 
-    file_path = "" 
-    tags = ""
+    file_path = None 
+    tags = None
+    
     def __init__(self, file_path) : 
         """ EXIFReader 클래스 생성자 """
 
@@ -29,53 +30,50 @@ class EXIFReader :
         # Return Exif tags
         self.tags = EXIF.process_file(f)        
 
-        
-    def get_thumbnails(self, file_path) :
-        """EXIF 정에서 썸네일을 읽는 것이 가능할 경우 썸네일을 읽은 후 리턴한다. """
-
-        print "ThumbNails"
 
     
     def get_geotag_lat(self):
         """EXIF 정보에서 GPS 정보를 읽어서 리턴한다.  """
+        result = None
+        
+        try: 
+            result = self.tags["GPS GPSLatitude"]
+        except KeyError, e:
+            result = "43, 4641/100, 0"
+            print "EXIF KeyError : No GPS GPSLatitude, Use default value. %s" % str(e)
 
-        return self.tags["GPS GPSLatitude"]
+
+        return result
     
 
     def get_geotag_lng(self):
         """EXIF 정보에서 GPS 정보를 읽어서 리턴한다.  """
+        result = None
+        
+        try: 
+            result = self.tags["GPS GPSLongitude"]
+        except KeyError, e: 
+            result = "11, 1533/100, 0"
+            print "EXIF KeyError : No GPS GPSLongitude, Use default value. %s" % str(e)
 
-        return self.tags["GPS GPSLatitude"]
+        return result
 
 
-    def print_all(self, file_path):
+    def get_taken_time(self):
+        """EXIF 정보에서 사진을 찍은 시간을 리턴한다  """
+
+        return self.tags["Image DateTime"]
+
+
+    def print_all(self):
         """모든 EXIF 정보를 STDOUT에 출력한다.  """
         
 
-        print "All Information of EXIF in " + file_path
+        print "All Information of EXIF in " + self.file_path
 
         for tag in self.tags.keys():
             if tag not in ('JPEGThumbnail', 'TIFFThumbnail', 'Filename', 'EXIF MakerNote'):
                 print "Key: %s, value %s" % (tag, self.tags[tag])
-
-
-    
-#All Information of EXIF in photos/2012-07-12 12.12.12.jpg
-#================================================
-#Key: GPS GPSLongitude, value [0, 843/100, 0]
-#Key: GPS GPSLatitude, value [51, 3003/100, 0]
-#Key: Image GPSInfo, value 594
-#Key: GPS GPSLatitudeRef, value N
-#Key: GPS GPSAltitudeRef, value 0
-#Key: GPS GPSTimeStamp, value [11, 12, 1181/100]
-#Key: GPS GPSAltitude, value 1820/317
-#Key: GPS GPSLongitudeRef, value W
-
-    
-
-
-
-
 
 
 
