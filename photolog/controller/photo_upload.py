1a3eaa4c951ae9713046e0db1bc5d0f08506c834
Keaton Hong <keatonh@gmail.com>
2013-07-14 18:16:39 +0900

    업로드 파일 확장자 검증 시 대소문자 구분없이 처리하도록 수정(소문자로 변환해서 검증)

--- photolog/controller/photo_upload.py
+++ photolog/controller/photo_upload.py
@@ -30,7 +30,7 @@ import uuid
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @photolog.route('/photo/upload')
