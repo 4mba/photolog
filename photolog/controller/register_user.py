18846442be0dd480f3afe99e0ca67dad4149429f
Keaton Hong <keatonh@gmail.com>
2013-07-23 14:44:23 +0900

    서비스 탈퇴 시, 업로드된 사진 파일도 일괄 삭제

--- photolog/controller/register_user.py
+++ photolog/controller/register_user.py
@@ -10,7 +10,8 @@
 """
 
 
-from flask import render_template, request, redirect , url_for, session
+import os
+from flask import render_template, request, redirect , url_for, session, current_app
 from sqlalchemy.exc import IntegrityError
 from werkzeug import generate_password_hash
 
@@ -21,13 +22,13 @@ from photolog.model.user import User
 from photolog.controller.login import login_required
 
 
-@photolog.route('/user/regist/', methods=['GET', 'POST'])
+@photolog.route('/user/regist', methods=['POST', 'GET'])
 def register_user():
     """포토로그 사용자 등록을 위한 함수"""
-    
+    print "%s invoked!" % request.method
     # HTTP POST로 요청이 오면 사용자 정보를 등록
     if request.method == 'POST':
-        
+
         password = request.form['password']
         password_confirm = request.form['password_confirm']
             
@@ -43,7 +44,7 @@ def register_user():
                 user = User(username, email, generate_password_hash(password))
                 dao.add(user)
                 dao.commit()
-    
+            
                 Log.debug(user) 
             except IntegrityError:
                 error = "아이디(%s)가 중복됩니다.다른 아이디를 사용세요." % username
@@ -116,17 +117,25 @@ def modify_user(username=None):
             return render_template('regist.html', user=user)
 
 
-@photolog.route('/user/unregist/')
+@photolog.route('/user/unregist')
 @login_required
 def unregist():
-    username = session['user_info'].username
+    user_id = session['user_info'].id
     try:
-        user = dao.query(User).filter_by(username=username).first()
-        if user.username == username:
+        user = dao.query(User).filter_by(id=user_id).first()
+        if user.id == user_id:
             dao.delete(user)
+            # 업로드된 사진 파일 삭
+            try:
+                upload_folder = os.path.join(current_app.root_path, 
+                                             current_app.config['UPLOAD_FOLDER'])
+                __delete_files(upload_folder, user.username)
+            except Exception as e:
+                Log.error("파일 삭제에 실패했습니다. 나중에 일괄 삭제하세요. : %s" + str(e))
+            
             dao.commit()
         else:
-            Log.error("존재하지 않는 사용자의 탈퇴시도 : %s", username)
+            Log.error("존재하지 않는 사용자의 탈퇴시도 : %d", user_id)
             raise Exception
     except Exception as e:
         Log.error(str(e))
@@ -135,4 +144,11 @@ def unregist():
     else:
         return redirect(url_for('.login'))
 
+def __delete_files(filepath, username):
+    import glob
+    del_filepath_rule = filepath  + username + "_*"
+    files = glob.glob(del_filepath_rule)
+    for f in files:
+        Log.debug(f)
+        os.remove(f)
 
