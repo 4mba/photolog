5219547d2b7a2757c6d68e141aa7e13853ad3db7
Keaton Hong <keatonh@gmail.com>
2013-07-23 14:41:55 +0900

    User 레코드 삭제 시, 관련된 Photo 레코드를 연쇄적으로 삭제하기 위한 관계 설정

--- photolog/model/user.py
+++ photolog/model/user.py
@@ -11,6 +11,7 @@
 
 
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 from photolog.model import Base
 
@@ -21,8 +22,10 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     username = Column(String(50), unique=True)
     email = Column(String(50), unique=True)
-    password = Column(String(120), unique=False)
+    password = Column(String(55), unique=False)
 
+    photos = relationship('Photo', backref='user', cascade='all, delete, delete-orphan')
+    
     def __init__(self, name, email, password):
         self.username = name
         self.email = email
