80f9b5a0be10e75017559ab0b6fe47d728c46bba
Keaton Hong <keatonh@gmail.com>
2013-07-24 03:24:02 +0900

    클래스 속성명 변경

--- photolog/photolog_logger.py
+++ photolog/photolog_logger.py
@@ -16,7 +16,7 @@ from logging import getLogger, handlers, Formatter
 
 
 class Log:
-    _log_level_map = {
+    __log_level_map = {
         'debug' : logging.DEBUG,
         'info' : logging.INFO,
         'warn' : logging.WARN,
@@ -24,51 +24,43 @@ class Log:
         'critical' : logging.CRITICAL
         }
     
-    _my_logger=None
+    __my_logger=None
     
     @staticmethod
     def init(logger_name='photolog', log_level='debug',
                     log_filepath='photolog/resource/log/photolog.log'):
-        Log._my_logger = getLogger(logger_name);
-        Log._my_logger.setLevel(Log._log_level_map.get(log_level, 'warn'))
+        Log.__my_logger = getLogger(logger_name);
+        Log.__my_logger.setLevel(Log.__log_level_map.get(log_level, 'warn'))
         
         formatter = Formatter('%(asctime)s - %(levelname)s - %(message)s')
             
         console_handler = logging.StreamHandler()
         console_handler.setFormatter(formatter)
-        Log._my_logger.addHandler(console_handler)
+        Log.__my_logger.addHandler(console_handler)
             
         file_handler = handlers.TimedRotatingFileHandler(log_filepath, when='D', interval=1)
         file_handler.setFormatter(formatter)
-        Log._my_logger.addHandler(file_handler)
-          
-#         return my_logger
+        Log.__my_logger.addHandler(file_handler)
     
     @staticmethod
     def debug(msg):
-        Log._my_logger.debug(msg)
+        Log.__my_logger.debug(msg)
     
     @staticmethod
     def info(msg):
-        Log._my_logger.info(msg)
+        Log.__my_logger.info(msg)
     
     @staticmethod
     def warn(msg):
-        Log._my_logger.warn(msg)
+        Log.__my_logger.warn(msg)
     
     @staticmethod
     def error(msg):
-        Log._my_logger.error(msg)
+        Log.__my_logger.error(msg)
     
     @staticmethod
     def critical(msg):
-        Log._my_logger.critical(msg)
-
-photolog_logger = None
-# 
-# from photolog import photolog_app
-# photolog_logger = _get_logger('photolog_logger'
-#                             , photolog_app.config['LOG_LEVEL'])
+        Log.__my_logger.critical(msg)
 
 
 
