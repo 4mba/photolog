37cae1d7d085f7154186615a2696620cc21be2c9
Keaton Hong <keatonh@gmail.com>
2013-07-23 14:43:04 +0900

    클래스 변수명 변경(내부 변수 형태로 변경:'__'를 접두어로 추가)

--- photolog/database.py
+++ photolog/database.py
@@ -17,22 +17,23 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 class DBManager:
     """데이터베이스 처리를 담당하는 공통 클래스"""
     
-    _engine = None
-    session = None
+    __engine = None
+    __session = None
 
     @staticmethod
     def init(db_url, db_log_flag=True):
-        DBManager._engine = create_engine(db_url, echo=db_log_flag, convert_unicode=True) 
-        DBManager.session = scoped_session(sessionmaker(autocommit=False, 
+        DBManager.__engine = create_engine(db_url, echo=db_log_flag) 
+        DBManager.__session = scoped_session(sessionmaker(autocommit=False, 
                                                         autoflush=False, 
-                                                        bind=DBManager._engine))
+                                                        bind=DBManager.__engine))
+
         global dao
-        dao = DBManager.session
+        dao = DBManager.__session
     
     @staticmethod
     def init_db():
         from photolog.model import *
         from photolog.model import Base
-        Base.metadata.create_all(bind=DBManager._engine)
+        Base.metadata.create_all(bind=DBManager.__engine)
 
 dao = None        
