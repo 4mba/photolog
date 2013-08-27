7a1721bd8a20574880a61dfc7f8964f650d5f73e
Keaton Hong <keatonh@gmail.com>
2013-08-27 20:13:48 +0900

    어플리케이션 객체명 변경 : app => application

--- runserver.py
+++ runserver.py
@@ -10,12 +10,16 @@
 """
 
 import sys
+
+from photolog import create_app
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+application = create_app()    
 
 if __name__ == '__main__':
     print "starting test server..."
-    from photolog import create_app
-    create_app().run(host='0.0.0.0', port=5000)
+
+    application.run(host='0.0.0.0', port=5000)
 
