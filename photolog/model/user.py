578cdd24c32f964f2ed4a48c115f1e47f79ce26a
Keaton Hong <keatonh@gmail.com>
2013-08-30 11:30:24 +0900

    email 필드 unique 속성 False로 변경 : 중복 허

--- photolog/model/user.py
+++ photolog/model/user.py
@@ -21,7 +21,7 @@ class User(Base):
 
     id = Column(Integer, primary_key=True)
     username = Column(String(50), unique=True)
-    email = Column(String(50), unique=True)
+    email = Column(String(50), unique=False)
     password = Column(String(55), unique=False)
 
     photos = relationship('Photo', backref='user', cascade='all, delete, delete-orphan')
