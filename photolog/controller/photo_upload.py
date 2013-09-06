290d04a25389ab748375580d66e58ecc31cfaf24
Keaton Hong <keatonh@gmail.com>
2013-09-06 17:27:11 +0900

    WTF validation 적용함 1) FileField에서 파일 및 데이터 검증이 안됨 2) 화면단에서 jquery로 사전검증을 하도록 이미 구현되있음

--- photolog/controller/photo_upload.py
+++ photolog/controller/photo_upload.py
@@ -16,13 +16,16 @@ from flask import request, redirect, url_for, current_app, render_template, sess
 from werkzeug.utils import secure_filename
 from datetime import datetime
 import uuid
+from PIL import Image
+from wtforms import Form, FileField, TextField, TextAreaField, HiddenField, \
+                    validators
 
 from photolog.database import dao
 from photolog.model.photo import Photo
 from photolog.controller.login import login_required
 from photolog.photolog_logger import Log
 from photolog.photolog_blueprint import photolog
-from PIL import Image
+
 
 
 ALLOWED_EXTENSIONS = set(['jpg', 'jpeg'])
@@ -37,7 +40,9 @@ def __allowed_file(filename):
 def upload_form():
     """ 사진파일을 업로드 하기 위해 업로드폼 화면으로 전환시켜주는 함수 """
     
-    return render_template('upload.html')
+    form = PhotoUploadForm(request.form)
+    
+    return render_template('upload.html', form=form)
 
 
 
@@ -46,65 +51,78 @@ def upload_form():
 def upload_photo():
     """ Form으로 파일과 변수들을 DB에 저장하는 함수. """
 
-    #: Form으로 넘어온 변수들의 값을 셋팅함
-    user_id = session['user_info'].id
-    username = session['user_info'].username
-    tag = request.form['tag']
-    comment = request.form['comment']
-    lat = request.form['lat']
-    lng = request.form['lng']
-    upload_date = datetime.today()
-
-    try:
-        #: Exif에서 전달받은 data 형식을 파이썬 date 객체로 변환
-        taken_date = datetime.strptime(request.form['date'], "%Y:%m:%d %H:%M:%S");
-    except :
-        #: date 포맷 예외 발생: exif가 없거나, 사진찍은 시간이 없는 경우에는 현재시간으로 대체 
-        taken_date = datetime.today()
+    form = PhotoUploadForm(request.form)
+        
+    # HTTP POST로 요청이 오면 사용자 정보를 등록
+    if form.validate():  
+        #: Session에 저장된 사용자 정보를 셋팅
+        user_id = session['user_info'].id
+        username = session['user_info'].username
+        
+        
+        #: Form으로 넘어온 변수들의 값을 셋팅함
+        tag = form.tag.data
+        comment =form.comment.data
+        lat = form.lat.data
+        lng = form.lng.data
+        upload_date = datetime.today()
     
-    #: 업로드되는 파일정보 값들을 셋팅한다.
-    upload_photo = request.files['upload']
-    filename = None
-    filesize = 0
-    filename_orig = upload_photo.filename
-
-    try:
-        #: 파일 확장자 검사 : 현재 jpg, jpeg만 가능
-        if upload_photo and __allowed_file(upload_photo.filename):
-            
-            ext = (upload_photo.filename).rsplit('.', 1)[1]
-
-            #: 유일하고 안전한 파일명을 얻는다.
-            filename = secure_filename(username +'_'+ unicode(uuid.uuid4())+"."+ext)
-            
-            upload_folder = os.path.join(current_app.root_path, current_app.config['UPLOAD_FOLDER'])
-            
-            upload_photo.save(os.path.join(upload_folder, filename))
-            
-            filesize = os.stat(upload_folder+filename).st_size
-            
-            #: 썸네일을 만든다.
-            make_thumbnails(filename)
-            
-        else:
-            raise Exception("File upload error : illegal file.")
-
-    except Exception as e:
-        Log.error(str(e))
-        raise e
-
-    try :
-        #: 사진에 대한 정보 DB에 저장
-        photo = Photo(user_id, tag, comment, filename_orig, filename, filesize, lat, lng, upload_date, taken_date)
-        dao.add(photo)
-        dao.commit()
-
-    except Exception as e:
-        dao.rollback()
-        Log.error("Upload DB error : " + str(e))
-        raise e
-
-    return redirect(url_for('.show_all'))
+        try:
+            #: Exif에서 전달받은 data 형식을 파이썬 date 객체로 변환
+            taken_date = datetime.strptime(form.taken_date.data, "%Y:%m:%d %H:%M:%S");
+        except :
+            #: date 포맷 예외 발생: exif가 없거나, 사진찍은 시간이 없는 경우에는 현재시간으로 대체 
+            taken_date = datetime.today()
+        
+        #: 업로드되는 파일정보 값들을 셋팅한다.
+        upload_photo = request.files['photo']
+        filename = None
+        filesize = 0
+        filename_orig = upload_photo.filename
+    
+        try:
+            #: 파일 확장자 검사 : 현재 jpg, jpeg만 가능
+            if upload_photo and __allowed_file(upload_photo.filename):
+                
+                ext = (upload_photo.filename).rsplit('.', 1)[1]
+    
+                #: 유일하고 안전한 파일명을 얻는다.
+                filename = secure_filename(username +'_'+ unicode(uuid.uuid4())+"."+ext)
+                
+                upload_folder = os.path.join(current_app.root_path, current_app.config['UPLOAD_FOLDER'])
+                
+                upload_photo.save(os.path.join(upload_folder, filename))
+                
+                filesize = os.stat(upload_folder+filename).st_size
+                
+                #: 썸네일을 만든다.
+                make_thumbnails(filename)
+                
+            else:
+                raise Exception("File upload error : illegal file.")
+    
+        except Exception as e:
+            Log.error(str(e))
+            raise e
+    
+        try :
+            #: 사진에 대한 정보 DB에 저장
+            photo = Photo(user_id, tag, comment, filename_orig, filename, filesize, lat, lng, upload_date, taken_date)
+            dao.add(photo)
+            dao.commit()
+    
+        except Exception as e:
+            dao.rollback()
+            Log.error("Upload DB error : " + str(e))
+            raise e
+    
+        return redirect(url_for('.show_all'))
+    else:
+        for key in form.errors.keys():
+            print key 
+            for error in form.errors[key]:
+                print error
+        return render_template('upload.html', form=form)
 
 
 
@@ -113,41 +131,46 @@ def upload_photo():
 def update_photo(photolog_id):
     """ 사진 업로드 화면에서 사용자가 수정한 내용을 DB에 업데이트 한다. """
 
-    #: 업데이트 대상 항목들
-    tag = request.form['tag']
-    comment = request.form['comment']
-    lat = request.form['lat']
-    lng = request.form['lng']
+    #: 변경전 원래의 photo 테이블 값을 읽어 온다.
+    photo = dao.query(Photo).filter_by(id=photolog_id).first()
+    form = PhotoUploadForm(request.form, photo)
+
+    if form.validate(): 
+        #: 업데이트 대상 항목들
+        tag = form.tag.data
+        comment = form.comment.data
+        lat = form.lat.data
+        lng = form.lng.data
+        
+        try :
+            #: 업데이트 값 셋팅
+            photo.tag = tag
+            photo.comment = comment
+            photo.geotag_lat = lat
+            photo.geotag_lng = lng
+            
+            dao.commit()
     
-    try :
-        #: 변경전 원래의 photo 테이블 값을 읽어 온다.
-        photo = dao.query(Photo).filter_by(id=photolog_id).first()
-        
-        #: 업데이트 값 셋팅
-        photo.tag = tag
-        photo.comment = comment
-        photo.geotag_lat = lat
-        photo.geotag_lng = lng
-        
-        dao.commit()
-
-    except Exception as e:
-        dao.rollback()
-        Log.error("Update DB error : " + str(e))
-        raise e
-
-    return redirect(url_for('.show_all'))
+        except Exception as e:
+            dao.rollback()
+            Log.error("Update DB error : " + str(e))
+            raise e
+    
+        return redirect(url_for('.show_all'))
+    else:
+        return render_template('upload.html', photo=photo, form=form)
 
 
 
-@photolog.route('/photo/modify/<photolog_id>')
+@photolog.route('/photo/update/<photolog_id>')
 @login_required
-def modify(photolog_id):
+def update_form(photolog_id):
     """ 업로드폼에서 입력한 값들을 수정하기 위해 DB값을 읽어와 업로드폼 화면으로 전달한다. """
     
     photo = dao.query(Photo).filter_by(id=photolog_id).first()
-    
-    return render_template('upload.html', photo=photo)
+    form = PhotoUploadForm(request.form, photo)
+        
+    return render_template('upload.html', photo=photo, form=form)
 
 
 
@@ -194,3 +217,13 @@ def make_thumbnails(filename):
     except Exception as e:
         Log.error("Thumbnails creation error : " + target_name+" , "+str(e))
         raise e
+    
+class PhotoUploadForm(Form):
+    """사진 등록 화면에서 사진 파일, 태그, 설명 경도, 위도, 사진 찍은 날짜을 검증함"""
+    
+    photo = FileField('Photo')
+    tag = TextField('Tag', [validators.Length(min=1, max=400, message='400자리 이하로 입력하세요.')])
+    comment = TextAreaField('Comment', [validators.Length(min=1, max=400, message='400자리 이하로 입력하세요.')])
+    lat = HiddenField('Latitude', [validators.Required(message='위치 정보(경도)가 없습니다.')])
+    lng = HiddenField('Longtitude', [validators.Required(message='위도 정보(위도)가 없습니다.')])
+    taken_date = HiddenField('Taken Date', [validators.Required(message='사진찍은 날짜가 없습니다.')])
